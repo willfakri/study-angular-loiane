771c51fc8c7423becf7b0a7dae5b539b0264a3e1
Willian Fakri Bratkauskas <wfakri@ciandt.com>
2023-03-24 14:40:48 -0300

    Organização das pastas e testes do tsc para o typescript

---
+++ 01-helloWorld/src/hellotype.js
@@ -0,0 +1,11 @@
+var var1 = 'texto';
+var num = 2;
+var PI = 3.14;
+function minhaFunc(x, y) {
+    return x + y;
+}
+var numeros = [1, 2, 3, 4, 5];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+numeros.map(function (valor) { return valor * 2; });
---
+++ 01-helloWorld/src/hellotype.ts
@@ -0,0 +1,15 @@
+var var1 = 'texto';
+
+let num = 2;
+const PI = 3.14;
+
+function minhaFunc(x, y){
+    return x + y;
+}
+
+var numeros = [1, 2, 3, 4, 5];
+numeros.map(function(valor){
+    return valor * 2;
+});
+
+numeros.map(valor => valor * 2);
