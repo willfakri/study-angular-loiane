fd77fc6ac7c782adb15b7cb81639b7f7965c4765
Willian Fakri Bratkauskas <wfakri@ciandt.com>
2023-03-24 15:07:41 -0300

    Treinando o comportamento de modulos com componentes

--- 01-helloWorld/src/app/app.module.ts
+++ 01-helloWorld/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
@@ -9,7 +10,8 @@ import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
   declarations: [
     AppComponent,
     MeuPrimeiroComponent,
-    MeuPrimeiro2Component
+    MeuPrimeiro2Component,
+    CursosComponent
   ],
   imports: [
     BrowserModule
