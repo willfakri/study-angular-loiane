29f952df22a4e3e61289da1a148d21ff27f05c65
Willian Fakri Bratkauskas <wfakri@ciandt.com>
2023-03-27 15:52:43 -0300

    Aula de templates, exercício ainda sem uso de diretivas.

--- 01-helloWorld/src/app/cursos/cursos.component.ts
+++ 01-helloWorld/src/app/cursos/cursos.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
-export class CursosComponent {
+export class CursosComponent implements OnInit {
+  
+  nomeCurso: string;
+
+  constructor(){
+    this.nomeCurso = 'Curso da marotagem e da malandragem do Angular 2.x+'
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
